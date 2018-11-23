8f223a03640e4828ccd541919d494bf5f8c23f92
nicklambourne <ndl93@live.com>
2018-11-23 18:26:12 +1000

    setup.py

--- setup.py
+++ setup.py
@@ -10,11 +10,9 @@ requirements = [
     'pydub',
 ]
 
-include_package_data = True
-
 setup(
     name='kaldi_helpers',
-    version='0.2',
+    version='0.3',
     packages=find_packages(),
     url='https://github.com/CoEDL/kaldi-helpers',
     install_requires=requirements,
